8d280cb008b7357806e19af46812720c814bfc55
Haralds Lūsis <lusisharalds@gmail.com>
2022-06-08 19:30:27 +0300

    Eksāmens izpildīts. Markuss Haralds Lūsis PB-It-Ne-2

---
+++ Test2_search/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test2_search")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test2_search")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("50ffaa98-3a34-4ac9-b79f-b3638abfffe5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        IWebDriver driver;
         public Form1()
         {
             InitializeComponent();
+            driver = new FirefoxDriver(@"C:\users\lusis\Desktop\EKA\Programmaturas izstrades tehnologijas\");
+            driver.Url = "https://www.ebay.com";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Searchkey = textBox1.Text;
+            IWebElement Searchbar = driver.FindElement(By.Id("gh-ac"));
+            Searchbar.SendKeys(Searchkey);
+            IWebElement Poga = driver.FindElement(By.Id("gh-btn"));
+            Poga.Click();
+            textBox2.Text = driver.Url;
+            richTextBox1.AppendText(textBox2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().Back();
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            driver.Close();
         }
     }
 }
