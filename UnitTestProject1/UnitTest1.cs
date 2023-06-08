d8585a023097f537ee8aaae9bc8ea31624de0407
vilmarsb <vilmars.dev@gmail.com>
2023-06-08 19:33:17 +0300

    Vilmars Baumanis EKA_PB_IT_Ne-2

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string DRIVER_DIR = @"C:\Visual-Studio\chromedriver";
+        IWebDriver driver;
+        private IWebDriver getDriver()
+        {
+            if (driver == null)
+            {
+                var chromeOptions = new ChromeOptions();
+                chromeOptions.AddArguments("headless");
+                driver = new ChromeDriver(DRIVER_DIR, chromeOptions);
+                return driver;
+            }
+            return driver;
+        }
+        [TestMethod]
+        public void Test1_field()
+        {
+            getDriver().Url = "https://www.ebay.com/";
+            IWebElement searchField = getDriver().FindElement(By.CssSelector("#gh-ac"));
+            Assert.IsTrue(searchField.Displayed);
+        }
+        [TestMethod]
+        public void Test2_search()
+        {
+            getDriver().Url = "https://www.ebay.com/";
+            IWebElement searchField = getDriver().FindElement(By.CssSelector("#gh-btn"));
+            Assert.IsTrue(searchField.Displayed);
+        }
+    }
+}
